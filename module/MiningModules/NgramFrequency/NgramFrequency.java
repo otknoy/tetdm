cd0f41379ca773c6558c2114dc890b2d21a01ef6
otknoy <otknoy@gmail.com>
2014-06-02 11:42:50 +0900

    Neighboring noun terms as ngram token
    Extract noun terms
    Calculate term frequency (distribution)

--- module/MiningModules/NgramFrequency/NgramFrequency.java
+++ module/MiningModules/NgramFrequency/NgramFrequency.java
@@ -4,6 +4,12 @@ import source.*;
 import source.TextData.*;
 
 import org.apache.commons.lang3.StringUtils;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Arrays;
+
 
 
 public class NgramFrequency extends MiningModule {
@@ -28,34 +34,87 @@ public class NgramFrequency extends MiningModule {
   public void initializeData() {
     resetData();
     
-    KeywordData[] keywords = text.keyword;
-    int length = text.keywordNumber;
 
-    String[] ngramTokens = new String[length-(n-1)];
+    String[] ngramTokens = ngram(n, getTerms());
+    
+    Map freqMap = frequency(ngramTokens);
+    String[] terms = (String[])freqMap.keySet().toArray(new String[0]);
+    Integer[] freqs = (Integer[])freqMap.values().toArray(new Integer[0]);
+
+    // convert Ingeter[] to int[]
+    int[] primitiveFreqs = new int[freqs.length];
+    for (int i = 0; i < primitiveFreqs.length; i++) {
+      primitiveFreqs[i] = freqs[i];
+      if (freqs[i] != 1) {
+	System.out.println(freqs[i]);
+      }
+    }
+
+    setDataStringArray(terms);
+    setDataIntegerArray(primitiveFreqs);
+  }
+
+  @Override
+  public void miningOperations(int optionNumber) {
+  }
+
+
+  private List<String> getTerms() {
+    // get sentences
+    SentenceData[] sentences = new SentenceData[text.sentenceNumber];
+    for (int i = 0; i < sentences.length; i++) {
+      sentences[i] = text.sentence[i];
+    }
+
+    // get terms
+    List<String> terms = new ArrayList<String>();
+    for (SentenceData s : sentences) {
+      int[] wordIDList = s.wordIDList;
+      int wordIDListLength = s.wordNumber;
+      for (int i = 0; i < wordIDListLength; i++) {
+	// If word is not keyword, word id is -1.
+	// And then it is ignored.
+	if (wordIDList[i] < 0) continue;
+
+	KeywordData keyword = text.keyword[wordIDList[i]];
+	// Add only noun wor
+	// If keyword is noun, part of speech id is 1.
+	if (keyword.partOfSpeech == 1) {
+	  terms.add(keyword.word);
+	}
+      }
+    }
+
+    return terms;
+  }
+
+  private String[] ngram(int n, List<String> terms) {
+    String[] ngramTokens = new String[terms.size()-(n-1)];
     for (int i = 0; i < ngramTokens.length; i++) {
 
+      // join n tokens
       String[] nTokens = new String[n];
       for (int j = 0; j < nTokens.length; j++) {
-	nTokens[j] = keywords[i+j].word;
+	nTokens[j] = terms.get(i+j);
       }
       
       ngramTokens[i] = StringUtils.join(nTokens, '-');
     }
 
-
-    System.out.println("################################");
-    for (String s : ngramTokens) {
-      System.out.println(s);
-    }
-
-    
-    setDataStringArray(ngramTokens);
-    int[] hoge = new int[ngramTokens.length];
-    for (int i = 0; i < hoge.length; i++) hoge[i] = 100;
-    setDataIntegerArray(hoge);
+    return ngramTokens;
   }
 
-  @Override
-  public void miningOperations(int optionNumber) {
+
+  private Map<String, Integer> frequency(String[] tokens) {
+    Map<String, Integer> freqMap = new HashMap<String, Integer>();
+
+    for (String t : tokens) {
+      if (!freqMap.containsKey(t)) {
+	freqMap.put(t, 0);
+      }
+      freqMap.put(t, freqMap.get(t)+1);
+    }
+
+    return freqMap;
   }
 }
