d4bbc5670f62d02a77202bc26896cdaca3282410
otknoy <otknoy@gmail.com>
2014-11-13 18:15:28 +0900

    Refine SticyEditor, refs #3
    1. Adding scrollable text area
    2. Save sticky button (doesn't work)

--- src/ui/graph/component/StickyEditor.java
+++ src/ui/graph/component/StickyEditor.java
@@ -1,8 +1,9 @@
 package ui.graph.component;
 
 import java.awt.Dimension;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JScrollPane;
 
@@ -11,21 +12,21 @@ public class StickyEditor extends JFrame {
 
   private Sticky sticky;
 
-  public static final int WIDHT  = 480;
-  public static final int HEIGHT = 480;
 
-
-  public StickyEditor(Sticky sticky, int x, int y) {
+  public StickyEditor(Sticky sticky) {
     this.sticky = sticky;
 
-    this.setBounds(x, y,StickyEditor.WIDHT, StickyEditor.HEIGHT);
+    this.setSize(480, 360);
+    this.setLayout(new BorderLayout());
 
-    JPanel p = new JPanel();
-    
-    JTextArea textArea = new JTextArea("test");
+    // scrollable text area
+    JTextArea textArea = new JTextArea(this.sticky.getText());
+    textArea.setLineWrap(true);
     JScrollPane scrollPane = new JScrollPane(textArea);
-    p.add(scrollPane);
-    
-    this.add(p);
+    this.add(scrollPane, BorderLayout.CENTER);
+
+    // save button
+    JButton saveButton = new JButton("save");
+    this.add(saveButton, BorderLayout.SOUTH);
   }
 }
