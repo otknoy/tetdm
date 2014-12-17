9b984567b932aa69ba7e9de696b9ab1a30b3dd49
otknoy <otknoy@gmail.com>
2014-12-17 10:17:59 +0900

    Toolbox sample

--- src/ui/toolbox/Toolbox.java
+++ src/ui/toolbox/Toolbox.java
@@ -7,5 +7,17 @@ import javax.swing.*;
 public class Toolbox extends JPanel {
 
   public Toolbox() {
+    JPanel p = new JPanel();
+    
+    p.setLayout(new FlowLayout());
+    for (int i = 0; i < 100; i++) {
+      p.add(new JButton("test" + (i+1)));
+    }
+
+
+    JScrollPane scrollpane = new JScrollPane(p,
+					     JScrollPane.VERTICAL_SCROLLBAR_NEVER,
+					     JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+    this.add(scrollpane);
   }
 }
