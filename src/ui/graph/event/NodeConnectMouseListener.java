3bb0b1f8b7a805e6db630d6a24f441ba95bb1047
otknoy <otknoy@gmail.com>
2014-12-18 12:44:01 +0900

    Connect/disconnect nodes by draging & dropping

--- src/ui/graph/event/NodeConnectMouseListener.java
+++ src/ui/graph/event/NodeConnectMouseListener.java
@@ -3,6 +3,8 @@ package ui.graph.event;
 import ui.Interface;
 import ui.graph.component.Node;
 
+import java.util.List;
+import java.util.ArrayList;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
@@ -10,8 +12,12 @@ import java.awt.event.MouseMotionListener;
 
 public class NodeConnectMouseListener implements MouseListener, MouseMotionListener {
 
+  private static final int threshold = 192;
+
   private final Interface parent;
   private final Node node;
+
+  private List<Node> connectableNodes;
 
 
   public NodeConnectMouseListener(Interface parent, Node node) {
@@ -20,28 +26,38 @@ public class NodeConnectMouseListener implements MouseListener, MouseMotionListe
   }
 
   
-  @Override
-  public void mouseClicked(MouseEvent e) {}
+  @Override public void mouseClicked(MouseEvent e) {}
+  @Override public void mousePressed(MouseEvent e) {}
+  @Override public void mouseReleased(MouseEvent e) {}
 
-  @Override  
-  public void mousePressed(MouseEvent e) {
+  @Override public void mouseEntered(MouseEvent e) {
+    List<Node> nodes = this.parent.getGraphInterface().getNodes();
+
+    this.connectableNodes = new ArrayList<Node>();
+    for (Node n : nodes) {
+      if (this.node.isConnectableTo(n)) {
+	this.connectableNodes.add(n);
+      }
+    }
   }
 
-  @Override  
-  public void mouseReleased(MouseEvent e) {
+  @Override public void mouseExited(MouseEvent e) {}
+
+  @Override public void mouseDragged(MouseEvent e) {
+    for (Node n : this.connectableNodes) {
+      if (this.node.distance(n) <= this.threshold) {
+	if (this.node.isConnectedTo(n)) continue;
+
+	this.node.connectsTo(n);
+      } else {
+	if (!this.node.isConnectedTo(n)) continue;
+
+	this.node.disconnectsTo(n);
+      }
+
+      this.parent.getGraphInterface().repaint();
+    }
   }
 
-  @Override
-  public void mouseEntered(MouseEvent e) {}
-
-  @Override
-  public void mouseExited(MouseEvent e) {}
-
-  
-  @Override  
-  public void mouseDragged(MouseEvent e) {
-  }
-  
-  @Override
-  public void mouseMoved(MouseEvent e) {}
+  @Override public void mouseMoved(MouseEvent e) { }
 }
