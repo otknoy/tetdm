964aac7df28719d3336d81ec8ebdce3ff5339321
otknoy <otknoy@gmail.com>
2014-12-26 19:42:26 +0900

    Enable remove module node button, refs #22

--- src/ui/graph/component/ModuleNode.java
+++ src/ui/graph/component/ModuleNode.java
@@ -5,6 +5,8 @@ import ui.graph.component.event.NodeRemoveListener;
 
 import java.awt.Color;
 import java.awt.Point;
+import java.awt.Insets;
+import java.awt.Dimension;
 import javax.swing.JButton;
 
 
@@ -12,15 +14,12 @@ public abstract class ModuleNode extends Node {
 
   protected ModuleData moduleData;
 
-  private final JButton removeButton;
-
 
   public ModuleNode(ModuleData moduleData, Color bgColor, Point location) {
     super(moduleData.getName(), moduleData.getId(), bgColor, location);
     this.moduleData = moduleData;
 
-    this.removeButton = new JButton("x");
-    this.add(this.removeButton);
+    this.setLayout(null);
   }
 
   public ModuleNode(ModuleData moduleData, Color bgColor) {
@@ -30,6 +29,10 @@ public abstract class ModuleNode extends Node {
   abstract public ModuleNode clone();
 
   public void addRemoveListener(NodeRemoveListener nodeRemoveListener) {
-    this.removeButton.addMouseListener(nodeRemoveListener);
+    JButton rmButton = new JButton("x");
+    rmButton.setSize(12, 12);
+    rmButton.setLocation(new Point(this.getWidth()  - rmButton.getWidth(), 0));
+    rmButton.addMouseListener(nodeRemoveListener);
+    this.add(rmButton);
   }
 }
