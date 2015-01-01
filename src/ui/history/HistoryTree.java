394d91c7a0984f4b6b33222081b6100546f37238
otknoy <otknoy@gmail.com>
2015-01-02 00:30:46 +0900

    temp

--- src/ui/history/HistoryTree.java
+++ src/ui/history/HistoryTree.java
@@ -1,12 +1,44 @@
 package ui.history;
 
-import java.awt.*;
-import javax.swing.*;
+import ui.history.component.History;
+
+import java.util.List;
+import java.util.ArrayList;
 
 
-public class HistoryTree extends JPanel {
+public class HistoryTree {
 
-  public HistoryTree() {
+  private final HistoryTreePanel parent;
+  private final History root;
+  private final List<History> historyList;
+  private History current;
 
+
+  public HistoryTree(HistoryTreePanel parent, History root) {
+    this.parent = parent;
+    this.root = root;
+    this.historyList = new ArrayList<History>();
+    this.current = this.root;
+  }
+
+  public HistoryTree(HistoryTreePanel parent) {
+    this(parent, null);
+  }
+
+
+  public History getCurrent() { return this.current; }
+
+  public void changeCurrent(History h) {
+    this.current = h;
+    this.parent.repaint();
+  }
+
+  public void addHistory(History h) {
+    historyList.add(h);
+    current.addToNext(h);
+    current = h;
+
+    this.parent.add(h);
+    this.parent.repaint();
   }
 }
---
+++ src/ui/history/HistoryTreePanel.java
@@ -0,0 +1,58 @@
+package ui.history;
+
+import ui.Interface;
+import ui.graph.GraphInterface;
+import ui.history.component.History;
+
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import java.awt.Point;
+
+
+public class HistoryTreePanel extends JPanel implements MouseListener {
+
+  static final String NAME = "History Tree";
+
+  private Interface parent;
+
+  private History root;
+  private HistoryTree historyTree;
+
+
+  public HistoryTreePanel(Interface parent) {
+    this.parent = parent;
+    this.historyTree = new HistoryTree(this);
+    
+    this.setLayout(null);
+  }
+
+
+  @Override public void paintComponent(Graphics g) {
+    g.setColor(Color.white);
+    g.fillRect(0, 0, getWidth(), getHeight());
+
+    // draw edges
+    // this.root.drawEdge(g);
+  }
+  
+  
+  @Override public void mouseClicked(MouseEvent e) {
+    // History h = (History)e.getSource();
+    // this.current.selected(false);
+    // this.current = h;
+    // this.current.selected(true);
+    // this.repaint();
+
+    // // change graph panel
+    // GraphPanel gp = ((GraphPanel)this.current.getData());
+    // this.moduleSelectPanel.changeGraphPanel(gp);
+  }
+  
+  @Override public void mousePressed(MouseEvent e) {}
+  @Override public void mouseReleased(MouseEvent e) {}
+  @Override public void mouseEntered(MouseEvent e) {}
+  @Override public void mouseExited(MouseEvent e) {}
+}
