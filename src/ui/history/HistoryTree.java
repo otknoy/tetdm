157e537a99570e8ea927f4375498a6136fbbae79
otknoy <otknoy@gmail.com>
2015-01-02 20:31:20 +0900

    Revemo unused list

--- src/ui/history/HistoryTree.java
+++ src/ui/history/HistoryTree.java
@@ -2,22 +2,17 @@ package ui.history;
 
 import ui.history.component.History;
 
-import java.util.List;
-import java.util.ArrayList;
-
 
 public class HistoryTree {
 
   private final HistoryTreePanel parent;
   private final History root;
-  private final List<History> historyList;
   private History current;
 
 
   public HistoryTree(HistoryTreePanel parent, History root) {
     this.parent = parent;
     this.root = root;
-    this.historyList = new ArrayList<History>();
     this.current = this.root;
   }
 
@@ -34,7 +29,6 @@ public class HistoryTree {
   }
 
   public void addHistory(History h) {
-    historyList.add(h);
     current.addToNext(h);
     current = h;
 
