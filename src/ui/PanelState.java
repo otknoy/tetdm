863508635e5915f400048b254db9778ab69dc664
otknoy <otknoy@gmail.com>
2015-01-08 17:40:02 +0900

    Implement PanelState class

---
+++ src/ui/PanelState.java
@@ -0,0 +1,15 @@
+package ui;
+
+public class PanelState {
+
+  private final int panelId;
+  private final int miningModuleId;
+  private final int visualizationModuleId;
+
+
+  private PanelState(int panelId, int miningModuleId, int visualizationModuleId) {
+    this.panelId = panelId;
+    this.miningModuleId = miningModuleId;
+    this.visualizationModuleId = visualizationModuleId;
+  }
+}
